30a1ef28f5aefbbd51d2b1cce71c8c03ded41f11
Ma Qiming <wisaly@gmail.com>
2013-01-08 23:33:31 +0800

    Add xml library

---
+++ WinEmoticons/WinEmoticons/Xml.h
@@ -0,0 +1,43 @@
+#pragma once
+#include "XmlNode.h"
+
+
+//-------------------------------------------------------------------------
+// Classes Name	    :CXml CXmlNode CXmlNodes
+// Author			:csdn[我就是传说中的。。。。SB]	sky-sea-earth@21cn.com 
+// CreateTime       :2005-10-07
+// Memo             :Xml结点类库
+// Version          :v1.0
+//-------------------------------------------------------------------------
+
+
+
+class CXml
+{
+public:
+	CXml(void);
+	~CXml(void);
+
+protected:
+	MSXML2::IXMLDOMDocument2Ptr m_pDoc; 
+	CString						m_strFilePath;
+
+public:
+	BOOL Open(CString strXmlFilePath);
+	BOOL Save(LPCTSTR lpszFilePath = NULL);
+	void Close(void);
+
+	BOOL Create(CString strXmlFilePath);	// add by mqm @ 2011.3.25
+
+	CString GetLastError(void);
+
+	CXmlNodePtr GetRoot(void);		
+
+
+	static BOOL IsFileExist(CString strFilePath);
+	CXmlNodePtr SelectSingleNode(LPCTSTR lpszPath);
+	CXmlNodesPtr SelectNodes(LPCTSTR lpszPath);
+
+	CString GetXmlText();					// add by mqm @ 2011.4.7
+	BOOL SetXmlText(LPCTSTR lpszXml);		// add by mqm @ 2011.4.7
+};
